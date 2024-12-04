73d8a24e29ae9ca8cfd8c93fe5ade61fa580c093
Gergő Ábrahám <abrahamgergo@gmail.com>
2024-12-04 19:11:12 +0100

    add example input file

---
+++ example.input.ts
@@ -0,0 +1,10 @@
+/**
+ * Copy this file next to the source files with the correct file name, e.g.
+ * ./src/13/13.input.ts
+ *
+ * and assign the puzzle input to the `input` variable.
+ */
+
+const input = ``;
+
+export default input;
