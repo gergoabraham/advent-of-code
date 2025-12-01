97729ac623b214a79b324bf02bb2385f036914b1
Gergő Ábrahám <abrahamgergo@gmail.com>
2025-12-01 21:55:13 +0100

    one repo: remove unnecessary files

--- scripts/solve.js
+++
@@ -1,6 +0,0 @@
-const problem = process.argv[2];
-const day = problem.match(/\d+/);
-const solver = require(`../src/${day}/${problem}.js`);
-const input = require(`../src/${day}/${day}.input.js`);
-
-console.log(solver(input));
